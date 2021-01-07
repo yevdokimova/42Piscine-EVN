8d9a2b15175fe472ad311bb950c5bed402b416dc
Mariam <mary.yevdokimova@gmail.com>
2021-01-08 01:27:44 +0400

    C06 project

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myevdoki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 11:11:06 by myevdoki          #+#    #+#             */
+/*   Updated: 2020/12/16 12:52:04 by myevdoki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_strcmp(char *str1, char *str2)
+{
+	int i;
+
+	i = 0;
+	while (str1[i] || str2[i])
+	{
+		if (str1[i] < str2[i])
+			return (-1);
+		if (str1[i] > str2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char *a[], char *b[])
+{
+	char *tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			ft_swap(&argv[i], &argv[i + 1]);
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+}
