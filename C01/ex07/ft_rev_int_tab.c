b11c2dd9568a48ff88d7bf4f92ced1c592e1c15d
Mariam <mary.yevdokimova@gmail.com>
2021-01-08 01:22:21 +0400

    C01 project

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myevdoki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/08 16:22:59 by myevdoki          #+#    #+#             */
+/*   Updated: 2020/12/08 19:11:53 by myevdoki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int index;
+	int tmp;
+
+	index = 0;
+	while (index < size / 2)
+	{
+		tmp = tab[index];
+		tab[index] = tab[size - 1 - index];
+		tab[size - 1 - index] = tmp;
+		index++;
+	}
+}
